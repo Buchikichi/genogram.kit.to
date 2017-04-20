cdb09d7e8a68291f9503ec97631ee8d8c8505920
ponta <ponta@WIN-HR9G6MRKSSQ>
2017-04-20 23:04:44 +0900

    Modify AppMain.

--- src/main/java/to/kit/genogram/AppMain.java
+++ src/main/java/to/kit/genogram/AppMain.java
@@ -2,12 +2,14 @@ package to.kit.genogram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * メイン.
  * @author H.Sasai
  */
 @SpringBootApplication
+@Configuration
 public class AppMain {
 	/**
 	 * メイン.
